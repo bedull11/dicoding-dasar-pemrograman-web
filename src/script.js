80ef1de3267ac1a91df9b0e7a288b35b04faf99d
bedull11 <abd.afs39@gmail.com>
2023-09-15 22:01:32 +0700

    section work dkk

--- src/script.js
+++ src/script.js
@@ -1,15 +1,35 @@
 feather.replace();
 
 // Menu toggle
-const hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector("#hamburger");
 
-hamburger.addEventListener('click', function (){
-    hamburger.classList.toggle('hamburger-active');
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+});
 
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav ul');
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav ul");
 
-menuToggle.addEventListener('click', function (){
-    nav.classList.toggle('slide');
-})
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+// Scroll Up
+const backTop = document.querySelector(".back-top");
+window.addEventListener("scroll", checkHeight);
+
+function checkHeight() {
+  if (window.scrollY > 88) {
+    backTop.style.display = "flex";
+  } else {
+    backTop.style.display = "none";
+  }
+}
+
+backTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+    // duration: 123
+  });
+});
